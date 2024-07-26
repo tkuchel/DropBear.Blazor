5480623c2241aa3ead97464ed41b278e80713b38
DropBear <tkuchel@users.noreply.github.com>
2024-07-26 16:27:19 +1000

    Updated base.css Added ShortWaitSpinner.razor Added LongWaitProgressBar.razor

---
+++ DropBear.Blazor/Components/Loaders/LongWaitProgressBar.razor.cs
@@ -0,0 +1,35 @@
+﻿#region
+
+using DropBear.Blazor.Enums;
+using Microsoft.AspNetCore.Components;
+
+#endregion
+
+namespace DropBear.Blazor.Components.Loaders;
+
+public partial class LongWaitProgressBar : ComponentBase
+{
+    [Parameter] public ThemeType Theme { get; set; } = ThemeType.DarkMode;
+    [Parameter] public string Title { get; set; } = "Long Wait";
+    [Parameter] public string Message { get; set; } = "Please wait while we process your request.";
+    [Parameter] public bool ShowCancelButton { get; set; } = true;
+    [Parameter] public int Progress { get; set; } = 0;
+
+    [Parameter] public EventCallback OnCancel { get; set; }
+
+    // Get css style based on theme
+    private string GetThemeCssClass()
+    {
+        return Theme switch
+        {
+            ThemeType.DarkMode => "theme-dark",
+            ThemeType.LightMode => "theme-light",
+            _ => "theme-dark"
+        };
+    }
+
+    private async Task HandleCancelClick()
+    {
+        await OnCancel.InvokeAsync();
+    }
+}
---
+++ DropBear.Blazor/Components/Loaders/ShortWaitSpinner.razor.cs
@@ -0,0 +1,26 @@
+﻿#region
+
+using DropBear.Blazor.Enums;
+using Microsoft.AspNetCore.Components;
+
+#endregion
+
+namespace DropBear.Blazor.Components.Loaders;
+
+public partial class ShortWaitSpinner : ComponentBase
+{
+    [Parameter] public ThemeType Theme { get; set; } = ThemeType.DarkMode;
+    [Parameter] public string Title { get; set; } = "Short Wait";
+    [Parameter] public string Message { get; set; } = "Please wait while we process your request.";
+
+    // Get css style based on theme
+    private string GetThemeCssClass()
+    {
+        return Theme switch
+        {
+            ThemeType.DarkMode => "theme-dark",
+            ThemeType.LightMode => "theme-light",
+            _ => "theme-dark"
+        };
+    }
+}
