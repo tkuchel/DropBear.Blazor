7d9be63ec6076d114b8dba1909b6bbc7a05660ae
DropBear <tkuchel@users.noreply.github.com>
2024-08-04 12:03:59 +1000

    Added new ValidationErrorsComponent.razor

---
+++ DropBear.Blazor/Components/Validation/ValidationErrorsComponent.razor.cs
@@ -0,0 +1,21 @@
+﻿#region
+
+using DropBear.Blazor.Components.Bases;
+using DropBear.Codex.Validation.ReturnTypes;
+using Microsoft.AspNetCore.Components;
+
+#endregion
+
+namespace DropBear.Blazor.Components.Validation;
+
+public sealed partial class ValidationErrorsComponent : DropBearComponentBase
+{
+    private bool _isCollapsed;
+
+    [Parameter] public ValidationResult? ValidationResult { get; set; }
+
+    private void ToggleCollapse()
+    {
+        _isCollapsed = !_isCollapsed;
+    }
+}
