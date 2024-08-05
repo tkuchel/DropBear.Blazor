383b5a96b0010b8b0aca7cee6879edcd69e7b257
DropBear <tkuchel@users.noreply.github.com>
2024-08-05 13:19:44 +1000

    Added new component DropBearList.razor

---
+++ DropBear.Blazor/Components/Lists/DropBearList.razor.cs
@@ -0,0 +1,24 @@
+﻿#region
+
+using DropBear.Blazor.Components.Bases;
+using Microsoft.AspNetCore.Components;
+
+#endregion
+
+namespace DropBear.Blazor.Components.Lists;
+
+public sealed partial class DropBearList<T> : DropBearComponentBase where T : class
+{
+    [Parameter] public IReadOnlyCollection<T>? Items { get; set; }
+    [Parameter] public string Title { get; set; } = string.Empty;
+    [Parameter] public string HeaderIcon { get; set; } = string.Empty;
+    [Parameter] public string HeaderColor { get; set; } = "#f44336"; // Default to a neutral red color
+    [Parameter] public RenderFragment<T> ItemTemplate { get; set; } = null!;
+
+    private bool IsCollapsed { get; set; }
+
+    private void ToggleCollapse()
+    {
+        IsCollapsed = !IsCollapsed;
+    }
+}
